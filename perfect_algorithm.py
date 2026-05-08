6ca71414234c1393ee3e7eac83588d78e5b101e8
RocioMaSala <martinrocio16@gmail.com>
2026-05-08 14:24:37 +0200

    mergetry2

--- perfect_algorithm.py
+++ perfect_algorithm.py
@@ -14,6 +14,7 @@ def is_valid(row: int, col: int, vis: list[list[bool]],
     if (vis[row][col]):
         return False
     return True
+    return True
 
 
 def dfs(maze: list[list[list[int]]], vis: list[list[bool]], height: int,
